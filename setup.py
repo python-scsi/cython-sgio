ecfc115c615b79296abd721453117a491ee4881e
Diego Elio Pettenò <flameeyes@flameeyes.com>
2020-03-30 12:42:37 +0100

    Initial import of the cython-sgio source tree.
    This is effectively just a fork out of the Cython sgio code from
    https://github.com/rosjat/python-scsi/pull/82 into its own module.
    
    The Cython code does not rely on any of the other python-scsi interfaces,
    and it's pretty much standalone.
    
    Since the code is pretty much copied-over, authorship is shared with the
    original authors.

---
+++ setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+#
+# SPDX-FileCopyrightText: © 2020 The cython-sgio Authors
+# SPDX-License-Identifier: LGPL-2.1+
+
+import sys
+
+from setuptools import Extension, find_packages, setup
+
+# Ensure it's present.
+import setuptools_scm  # noqa: F401
+from Cython.Build import cythonize
+
+configured_extensions = []
+
+if sys.platform == "linux":
+    configured_extensions.append(Extension("sgio", ["src/linux_sgio.pyx"]))
+
+if not configured_extensions:
+    raise NotImplementedError("No SGIO implemented for " + sys.platform)
+
+
+setup(
+    packages=find_packages(),
+    package_data={"": ["*.pyx", "*.pxd"]},
+    ext_modules=cythonize(configured_extensions),
+    extras_require={
+        "dev": [
+            "Cython",
+            "mypy",
+            "pre-commit",
+            "setuptools>=42",
+            "setuptools_scm[toml]>=3.4",
+            "wheel",
+        ]
+    },
+)
