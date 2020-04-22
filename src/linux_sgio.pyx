2c06b4cc13d2688379bcbff2f74c7ae6f8ba3c1e
Diego Elio Pettenò <flameeyes@flameeyes.com>
2020-04-22 01:21:21 +0100

    Make sure to copy the _whole_ sense buffer to Python.
    When a Check Condition is identified, the sense buffer needs to be copied
    from Cython to Python, and is then embedded in the `CheckConditionError`
    exception.
    
    But to make sure the copy includes the whole content, we need to explicitly
    ask for the `sb_len_wr` amount of bytes.

--- src/linux_sgio.pyx
+++ src/linux_sgio.pyx
@@ -24,7 +24,7 @@ See Also:
 from cpython.bytearray cimport PyByteArray_FromStringAndSize
 
 from libc.errno cimport errno
-from libc.stdlib cimport calloc
+from libc.stdlib cimport calloc, free
 from posix.ioctl cimport ioctl
 
 cdef extern from "scsi/sg.h":
@@ -90,56 +90,60 @@ def execute(
         bytearray data_in,
 ):
     cdef sg_io_hdr_t io_hdr
+    cdef unsigned char[:] input_view = data_in
+
     cdef unsigned char *sense = <unsigned char *> calloc(
         32, sizeof(unsigned char))
-    cdef unsigned char[:] input_view = data_in
     if not sense:
         raise MemoryError()
 
-    # Prepare the sg device I/O header structure.
-    io_hdr.interface_id = b'S'
-    io_hdr.cmd_len = len(cdb)
-    io_hdr.iovec_count = 0
-    io_hdr.cmdp = cdb
-    io_hdr.sbp = sense
-    io_hdr.timeout = 1800000
-    io_hdr.flags = 0
-    io_hdr.mx_sb_len = 32  # we allocate it ourselves.
+    try:
+        # Prepare the sg device I/O header structure.
+        io_hdr.interface_id = b'S'
+        io_hdr.cmd_len = len(cdb)
+        io_hdr.iovec_count = 0
+        io_hdr.cmdp = cdb
+        io_hdr.sbp = sense
+        io_hdr.timeout = 1800000
+        io_hdr.flags = 0
+        io_hdr.mx_sb_len = 32  # we allocate it ourselves.
 
-    if data_out is not None:
-        data_out_len = len(data_out)
-    else:
-        data_out_len = 0
-
-    if data_in is not None:
-        data_in_len = len(data_in)
-    else:
-        data_in_len = 0
-
-    if data_out_len and data_in_len:
-        raise NotImplemented('Indirect IO is not supported.')
-    elif data_out_len:
-        io_hdr.dxfer_direction = SG_DXFER_TO_DEV
-        io_hdr.dxfer_len = data_out_len
-        io_hdr.dxferp = data_out
-    elif data_in_len:
-        io_hdr.dxfer_direction = SG_DXFER_FROM_DEV
-        io_hdr.dxfer_len = data_in_len
-        io_hdr.dxferp = &input_view[0]
-    else:
-        io_hdr.dxfer_len = 0
-        io_hdr.dxferp = NULL
-        io_hdr.dxfer_direction = SG_DXFER_NONE
-
-    result = ioctl(fid.fileno(), SG_IO, &io_hdr)
-    if result < 0:
-        raise OSError(errno, 'ioctl failed')
-
-    if io_hdr.info & SG_INFO_OK_MASK != SG_INFO_OK:
-        if io_hdr.sb_len_wr > 0:
-            raise CheckConditionError(sense)
+        if data_out is not None:
+            data_out_len = len(data_out)
         else:
-            raise UnspecifiedError()
+            data_out_len = 0
 
-    # Return the actual transfer written.
-    return io_hdr.resid
+        if data_in is not None:
+            data_in_len = len(data_in)
+        else:
+            data_in_len = 0
+
+        if data_out_len and data_in_len:
+            raise NotImplemented('Indirect IO is not supported.')
+        elif data_out_len:
+            io_hdr.dxfer_direction = SG_DXFER_TO_DEV
+            io_hdr.dxfer_len = data_out_len
+            io_hdr.dxferp = data_out
+        elif data_in_len:
+            io_hdr.dxfer_direction = SG_DXFER_FROM_DEV
+            io_hdr.dxfer_len = data_in_len
+            io_hdr.dxferp = &input_view[0]
+        else:
+            io_hdr.dxfer_len = 0
+            io_hdr.dxferp = NULL
+            io_hdr.dxfer_direction = SG_DXFER_NONE
+
+        result = ioctl(fid.fileno(), SG_IO, &io_hdr)
+        if result < 0:
+            raise OSError(errno, 'ioctl failed')
+
+        if io_hdr.info & SG_INFO_OK_MASK != SG_INFO_OK:
+            if io_hdr.sb_len_wr > 0:
+                raise CheckConditionError(bytes(sense[:io_hdr.sb_len_wr]))
+            else:
+                raise UnspecifiedError()
+
+        # Return the actual transfer written.
+        return io_hdr.resid
+    finally:
+        free(sense)
